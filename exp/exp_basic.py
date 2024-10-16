755db6cdbad99b04e3be172ad6caac19daeda528
bryzhu <bzhu1945@gmail.com>
2024-10-16 04:50:31 +0000

    add new exp_basic

--- exp/exp_basic.py
+++ exp/exp_basic.py
@@ -1,13 +1,23 @@
 import os
 import torch
-
+from utils.constants import mode
 
 class Exp_Basic(object):
     def __init__(self, args, model_name):
         self.args = args
         self.device = self._acquire_device()
         self.name = model_name
+        self.model_name = model_name.split('_')[0]
         self.model = self._build_model().to(self.device)
+        if self.args.no_dim_extension:
+          self.ext = 'noext'
+        else:
+          self.ext = args.embed_size
+        if args.delta_forecast:
+          self.delta = "delta"
+        else:
+          self.delta = "nodelta"
+        self.mode = mode[args.mode]
 
     def _build_model(self):
         raise NotImplementedError
